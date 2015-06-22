32a02fe30ad2a2c9601351e404fe3706c941572e
Ben Chang <changb3@rpi.edu>
2015-06-22 01:04:07 -0400

    got the scene working again, fixed wand rotation errors, updated center screen position for current screen configuration, removed odd .8m offsets in the projection root and pointer root

---
+++ Assets/WandScript2.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WandScript2 : MonoBehaviour {
+
+	public Transform pointer;
+	public Vector3 positionOffset = new Vector3 (0f,0f,.5f);
+	// Use this for initialization
+	void Start () {
+		pointer = GameObject.Find("PointerRoot").transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 wandPos = pointer.localPosition;
+		Vector3 wandRot = pointer.localEulerAngles;
+
+		transform.localPosition = wandPos + positionOffset;
+
+		float rx = wandRot.x;
+		float ry = wandRot.y;
+		float rz = wandRot.z;
+
+		//rx=-rx;
+		//ry=-ry;
+		//rz=-rz;
+		transform.localEulerAngles = new Vector3 (rx,ry,rz);
+	}
+}
